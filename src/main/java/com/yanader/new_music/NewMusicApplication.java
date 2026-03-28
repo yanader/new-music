94e8b7f03de067f8b41f2de17b783bf9da543dca
ste <mrbarrow@hotmail.co.uk>
2026-03-28 17:32:40 +0000

    initial commit

---
+++ src/main/java/com/yanader/new_music/NewMusicApplication.java
@@ -0,0 +1,13 @@
+package com.yanader.new_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanader/new_music/NewMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanader.new_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
