9dd73979e4086749e35f5513d7d23c0e1bd5b3e3
ste <mrbarrow@hotmail.co.uk>
2026-03-29 19:46:42 +0100

    feat:Artist

--- src/main/java/com/yanader/new_music/entity/Artist.java
+++ src/main/java/com/yanader/new_music/entity/Artist.java
@@ -1,4 +1,23 @@
 package com.yanader.new_music.entity;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name="artists")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String spotify_artist_id;
 }
