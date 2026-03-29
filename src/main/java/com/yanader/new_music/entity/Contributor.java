93e84405ac741eea00679607544e922275072d1f
ste <mrbarrow@hotmail.co.uk>
2026-03-29 19:42:26 +0100

    feat:Contributor

--- src/main/java/com/yanader/new_music/entity/Contributor.java
+++ src/main/java/com/yanader/new_music/entity/Contributor.java
@@ -1,4 +1,24 @@
 package com.yanader.new_music.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "contributors")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Contributor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String alias;
+
+
 }
