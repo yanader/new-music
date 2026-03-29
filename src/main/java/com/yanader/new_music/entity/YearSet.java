797835efe29f774c3fc9133f698e17aeda278c0b
ste <mrbarrow@hotmail.co.uk>
2026-03-29 19:49:26 +0100

    feat:YearSet

--- src/main/java/com/yanader/new_music/entity/YearSet.java
+++ src/main/java/com/yanader/new_music/entity/YearSet.java
@@ -1,4 +1,24 @@
 package com.yanader.new_music.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name="year_sets")
+@AllArgsConstructor
+@NoArgsConstructor
 public class YearSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="listening_year")
+    private Integer listeningYear;
+    @Column(name="release_year")
+    private Integer releaseYear;
 }
