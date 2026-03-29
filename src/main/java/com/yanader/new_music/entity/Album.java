71699da253f56a6413f93ccc6080ee233b4928ad
ste <mrbarrow@hotmail.co.uk>
2026-03-29 21:29:54 +0100

    feat:Album

--- src/main/java/com/yanader/new_music/entity/Album.java
+++ src/main/java/com/yanader/new_music/entity/Album.java
@@ -1,4 +1,32 @@
 package com.yanader.new_music.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Entity
+@Table(name="albums")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="artist_id")
+    private Long artistId;
+    @Column(name="set_id")
+    private Long setId;
+    private Integer rating;
+    private String notes;
+    @Column(name="listened_on")
+    private LocalDate listenedOn;
+    @Column(name="spotify_album_id")
+    private String spotifyAlbumId;
 }
