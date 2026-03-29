16ab1f98b056db8ea99119b05f072676c61d79c6
ste <mrbarrow@hotmail.co.uk>
2026-03-29 21:32:18 +0100

    feat:Suggestion

--- src/main/java/com/yanader/new_music/entity/Suggestion.java
+++ src/main/java/com/yanader/new_music/entity/Suggestion.java
@@ -1,4 +1,24 @@
 package com.yanader.new_music.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name="suggestions")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Suggestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="contributor_id")
+    private Long contributorId;
+    @Column(name="album_id")
+    private Long albumId;
 }
