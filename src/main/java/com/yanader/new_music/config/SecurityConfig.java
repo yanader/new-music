8d69f52d3348d5a8a7f2698c37a23dbd501e1ffc
ste <mrbarrow@hotmail.co.uk>
2026-04-07 16:36:31 +0100

    feat:temp security chain

---
+++ src/main/java/com/yanader/new_music/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.yanader.new_music.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/albums/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/test/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/test/**").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
