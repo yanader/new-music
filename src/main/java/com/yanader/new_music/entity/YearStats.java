d1f6f63b2e4dba3ee1713be043befa5ff9813ed6
ste <mrbarrow@hotmail.co.uk>
2026-04-10 15:46:39 +0100

    feat:StatUtil

--- src/main/java/com/yanader/new_music/entity/YearStats.java
+++ src/main/java/com/yanader/new_music/entity/YearStats.java
@@ -1,5 +1,6 @@
 package com.yanader.new_music.entity;
 
+import com.yanader.new_music.utils.StatsUtil;
 import lombok.Getter;
 
 import java.util.HashMap;
@@ -9,8 +10,8 @@ import java.util.Objects;
 
 @Getter
 public class YearStats {
-    private Integer year;
-    private Integer count;
+    private final Integer year;
+    private final Integer count;
     private Integer countSevenPlus;
     private Double percentSevenPlus;
     private Double mean;
@@ -31,63 +32,18 @@ public class YearStats {
     }
 
     private void processAlbums(List<Album> albums) {
-        int sum = 0;
         List<Album> filteredAlbums = albums.stream().filter(album -> album.getRating() != null).toList();
-        Map<Integer, Integer> scores = new HashMap<>();
+        List<Integer> ratings = filteredAlbums.stream().map(Album::getRating).toList();
 
-        for (Album album: filteredAlbums) {
-            Integer rating = album.getRating();
+        for (Integer rating: ratings) {
             if (rating >= 7) {
                 this.countSevenPlus++;
             }
-            sum += rating;
-            scores.put(rating, scores.getOrDefault(rating, 0) + 1);
         }
         this.percentSevenPlus = this.count == 0 ? 0.0 : this.countSevenPlus * 1.0 / this.count;
-
-        this.mean = sum * 1.0 / this.count;
-
-        int tempMode = -1;
-        int maxCount = -1;
-
-        for (Map.Entry<Integer, Integer> entry : scores.entrySet()) {
-            if (entry.getValue() > maxCount) {
-                maxCount = entry.getValue();
-                tempMode = entry.getKey();
-            }
-        }
-
-        this.mode = tempMode;
-        this.median = median(filteredAlbums);
-        this.stdDev = stdDev(filteredAlbums);
-
+        this.mean = StatsUtil.mean(ratings);
+        this.mode = StatsUtil.mode(ratings);
+        this.median = StatsUtil.median(ratings);
+        this.stdDev = StatsUtil.stdDev(ratings);
     }
-
-    public Double median(List<Album> albums) {
-        List<Integer> ratings = albums.stream().map(Album::getRating).sorted().toList();
-
-        int size = ratings.size();
-        if (size == 0) return 0.0;
-        if (size % 2 == 1) {
-            return ratings.get(size/2) * 1.0;
-        } else {
-            return (ratings.get(size / 2 - 1) + ratings.get(size / 2)) / 2.0;
-        }
-    }
-
-    private Double stdDev(List<Album> albums){
-        List<Integer> ratings = albums.stream().map(Album::getRating).toList();
-        if (ratings.size() < 2) return 0.0;
-
-        double mean = ratings.stream().mapToDouble(i -> i).average().orElse(0.0);
-
-        double variance = 0.0;
-        for (int num : ratings) {
-            variance += Math.pow(num - mean, 2);
-        }
-        variance /= (ratings.size() - 1);
-
-        return Math.sqrt(variance);
-    }
-
 }
---
+++ src/main/java/com/yanader/new_music/utils/StatsUtil.java
@@ -0,0 +1,66 @@
+package com.yanader.new_music.utils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class StatsUtil {
+
+    private StatsUtil(){}
+
+    public static double mean(List<Integer> nums){
+        if (nums.isEmpty()) return 0.0;
+        int sum = 0;
+        for (Integer num: nums) {
+            sum += num;
+        }
+        return sum * 1.0 / nums.size();
+    }
+
+    public static double median(List<Integer> nums){
+        List<Integer> sorted = nums.stream().sorted().toList();
+        int size = nums.size();
+        if (size == 0) return 0.0;
+        if (size % 2 == 1) {
+            return sorted.get(size/2) * 1.0;
+        } else {
+            return (sorted.get(size / 2 - 1) + sorted.get(size / 2)) / 2.0;
+        }
+    }
+
+
+    public static int mode(List<Integer> nums){
+        Map<Integer, Integer> scores = new HashMap<>();
+
+        for (Integer num: nums) {
+            scores.put(num, scores.getOrDefault(num, 0) + 1);
+        }
+
+        int mode = -1;
+        int count = -1;
+
+        for (Map.Entry<Integer, Integer> entry : scores.entrySet()) {
+            if (entry.getValue() > count) {
+                count = entry.getValue();
+                mode = entry.getKey();
+            }
+        }
+        return mode;
+    }
+
+    public static double stdDev(List<Integer> nums){
+
+        if (nums.size() < 2) return 0.0;
+
+        double mean = mean(nums);
+
+        double variance = 0.0;
+        for (int num : nums) {
+            variance += Math.pow(num - mean, 2);
+        }
+        variance /= (nums.size() - 1);
+
+        return Math.sqrt(variance);
+    }
+}
